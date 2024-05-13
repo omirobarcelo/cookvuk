58387f4216875ba84ded16dcec5d30cbaf7bc02d
omirobarcelo <magna.OMIRO.shogun@gmail.com>
2024-05-13 18:33:06 +0200

    Removed default styles, added basic header

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header class="header">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <nav class="nav">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+}
+</style>
